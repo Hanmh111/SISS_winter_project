6d8b7d27112611f465960224c70945497cdc143b
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-01-29 04:09:32 +0900

    Create gamestart.php

---
+++ gamestart.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SISS Winter Projext</title>
+      <link rel="stylesheet" type="text/css" href="#"/>
+      <script type="text/javascript" src=""></script>
+  </head>
+  <body>
+    <div><a href="index.php">home</a></div>
+    <h1>문제</h1>
+  </body>
+</html>
