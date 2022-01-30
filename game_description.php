48742c985a666209a7e933c25cda118806718ee5
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-01-30 21:14:09 +0900

    Create game_description.php

---
+++ game_description.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SISS Winter Projext</title>
+      <link rel="stylesheet" type="text/css" href="#"/>
+      <script type="text/javascript" src=""></script>
+  </head>
+  <body>
+    <div><a href="index.php">home</a></div>
+    <h1>게임 설명</h1>
+    <p>
+      게임 설명 내용
+    </p>
+  </body>
+</html>
