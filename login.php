4520783ab4ec8913add17e25a3805337522c7e0e
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-01-30 21:15:02 +0900

    Create login.php

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SISS Winter Projext</title>
+      <link rel="stylesheet" type="text/css" href="#"/>
+      <script type="text/javascript" src=""></script>
+  </head>
+  <body>
+    <h1><a href="index.php">추리 게임</a></h1>
+    <h2>로그인</h2>
+    <form action="process_login.php" method="POST">
+      ID : <input type="text" name="id"/></br>
+      PW : <input type="password" name="pw"/></br>
+      <input type="submit" name="login" value="로그인">
+      <a href=sign_in.php>회원가입</a>
+    </form>
+
+  </body>
+</html>
