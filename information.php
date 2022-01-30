e245fe9d1e91503b51e5ff118e1da3ce8c7c77aa
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-01-30 21:14:49 +0900

    Create information.php

---
+++ information.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SISS Winter Projext</title>
+      <link rel="stylesheet" type="text/css" href="#"/>
+      <script type="text/javascript" src=""></script>
+  </head>
+  <body>
+    <div><a href="index.php">home</a></div>
+    <div><a href="community.php"><h1>커뮤니티</h1></a></div>
+    <h3>문제 제보</h3>
+    <form action="process_information.php" method="POST">
+      <p><input type="text" name="title" placeholder="제목"></p>
+      <p><textarea name="description" rows="8" cols="80" placeholder="문제를 제보해주셔서 감사합니다."></textarea></p>
+      <p><input type="submit"></p>
+  </body>
+</html>
