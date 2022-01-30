092b4e8f5019d9b2f0c305b408ca74324b6ca247
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-01-30 21:17:11 +0900

    Create sign_in.php

---
+++ sign_in.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SISS Winter Projext</title>
+      <link rel="stylesheet" type="text/css" href="#"/>
+      <script type="text/javascript" src=""></script>
+  </head>
+  <body>
+    <h1><a href="index.php">추리 게임</a></h1>
+    <h2>회원 가입</h2>
+    <form action="process_sign_up.php" method="post">
+      <h4>Id</h4>
+      <input type="text" name="login_id" value="" placeholder="Id를 입력해주세요.">
+      <h4>Pw</h4>
+      <input type="password" name="login_pw" placeholder="PW를 입력해주세요.">
+    </br>
+      <input type="submit" name="Sign Up" value="Sign Up">
+    </form>
+  </body>
+</html>
