526c5a61eb2480ff86aaf0120b68712fc60ec018
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:32:51 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,31 +7,38 @@ $total=mysqli_num_rows($result);
 
 session_start();
 if(isset($_SESSION['is_logged'])){
-  $is_logged = $_SESSION['is_logged'];
-  echo $_SESSION['userid'],"님 환영합니다.</br>";
+  $is_logged = $_SESSION['is_logged']; ?>
+  <div style="color: white;"><?php
+  echo $_SESSION['userid'],"님 환영합니다.</br>"; ?></div><?php
   echo "<a href=process_logout.php>로그아웃</a>";
-
 }
 else{
-  echo "로그인 먼저 해주세요";
+  ?>
+  <div style="color: white;"><?php
+  echo "로그인 먼저 해주세요"; ?></div>
+  <div class="right">
+    <a href="sign_in.php">회원가입</a>
+    <a href="login.php">로그인</a>
+  </div>  <?php
 ;}
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>SISS Winter Projext</title>
       <link rel="stylesheet" type="text/css" href="#"/>
       <script type="text/javascript" src=""></script>
   </head>
   <body>
+  <div class="centering">
     <h1>추리 게임</h1>
-    <a href="gamestart.php">게임 시작</a>
-    <a href="game_description.php">게임 설명</a>
-    <a href="community.php">커뮤니티</a>
-    <a href="sign_in.php">회원가입</a>
-    <a href="login.php">로그인</a>
-
+    <img src="banner.jpg" width="550" height="500"><p>
+    <a href="gamestart.php">게임시작 &emsp;</a>
+    <a href="game_description.php">게임설명 &emsp;</a>
+  <a href="community.php">커뮤니티 &emsp;</a>
+  </div>
   </body>
 </html>
