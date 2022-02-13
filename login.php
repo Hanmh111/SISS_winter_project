9f2fc3b3ba57e54e3bf7c208d84ef6cca5f017ca
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:33:26 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>SISS Winter Projext</title>
       <link rel="stylesheet" type="text/css" href="#"/>
       <script type="text/javascript" src=""></script>
   </head>
   <body>
-    <h1><a href="index.php">추리 게임</a></h1>
+    <div class="centering"><a href="index.php"><img src="banner.jpg" width="150" height="150"></a>
+    <h1>추리 게임</a></h1>
     <h2>로그인</h2>
+    <div class="centering">
     <form action="process_login.php" method="POST">
       ID : <input type="text" name="id"/></br>
-      PW : <input type="password" name="pw"/></br>
+      PW : <input type="password" name="pw"/></p>
       <input type="submit" name="login" value="로그인">
       <a href=sign_in.php>회원가입</a>
     </form>
-
+  </div>
   </body>
 </html>
