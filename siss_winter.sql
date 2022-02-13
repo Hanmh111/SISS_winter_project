c9c3766a75fff16635b3a345969ec0bffec39fe1
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:37:35 +0900

    Update siss_winter.sql

--- siss_winter.sql
+++ siss_winter.sql
@@ -1,13 +1,240 @@
+CREATE DATABASE  IF NOT EXISTS `siss_winter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `siss_winter`;
 -- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: siss_winter
+-- Host: 127.0.0.1    Database: siss_winter
 -- ------------------------------------------------------
 -- Server version	10.4.22-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `privacy`
+--
+
+DROP TABLE IF EXISTS `privacy`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `privacy` (
+  `id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pw` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `profile` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `privacy`
+--
+
+LOCK TABLES `privacy` WRITE;
+/*!40000 ALTER TABLE `privacy` DISABLE KEYS */;
+/*!40000 ALTER TABLE `privacy` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-12 23:49:15
+CREATE DATABASE  IF NOT EXISTS `siss_winter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `siss_winter`;
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: siss_winter
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gamelogin`
+--
+
+DROP TABLE IF EXISTS `gamelogin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gamelogin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `login_pw` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `level` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gamelogin`
+--
+
+LOCK TABLES `gamelogin` WRITE;
+/*!40000 ALTER TABLE `gamelogin` DISABLE KEYS */;
+INSERT INTO `gamelogin` VALUES (1,'admin','0000','2022-01-21 19:14:13',6),(2,'user1','1111','2022-02-04 19:43:27',1);
+/*!40000 ALTER TABLE `gamelogin` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-12 23:49:14
+CREATE DATABASE  IF NOT EXISTS `siss_winter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `siss_winter`;
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: siss_winter
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gamelevel`
+--
+
+DROP TABLE IF EXISTS `gamelevel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gamelevel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `answer` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firsthint` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `secondhint` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gamelevel`
+--
+
+LOCK TABLES `gamelevel` WRITE;
+/*!40000 ALTER TABLE `gamelevel` DISABLE KEYS */;
+INSERT INTO `gamelevel` VALUES (1,'1번','1번 문제','2022-02-04 23:13:28','정답','1-1번 힌트','1-2번 힌트'),(2,'2번','2번 문제','2022-02-04 23:14:22','정답','2-1번 힌트','2-2번 힌트'),(3,'3번','3번 문제','2022-02-04 23:14:26','정답','3-1번 힌트','3-2번 힌트'),(4,'4번','4번 문제','2022-02-04 23:14:30','정답','4-1번 힌트','4-2번 힌트'),(5,'5번','5번 문제','2022-02-04 23:14:33','정답','5-1번 힌트','5-2번 힌트'),(6,'6번','6번 문제','2022-02-04 23:14:39','정답','6-1번 힌트','6-2번 힌트');
+/*!40000 ALTER TABLE `gamelevel` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-12 23:49:14
+CREATE DATABASE  IF NOT EXISTS `siss_winter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `siss_winter`;
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: siss_winter
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `community_text`
+--
+
+DROP TABLE IF EXISTS `community_text`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `community_text` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp(),
+  `author` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `spoiler` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT 'x',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `community_text`
+--
+
+LOCK TABLES `community_text` WRITE;
+/*!40000 ALTER TABLE `community_text` DISABLE KEYS */;
+INSERT INTO `community_text` VALUES (3,'mysql','mysql is...','2022-01-21 05:35:00','Anonymous','x'),(4,'Oracle','Oracle is ...','2022-01-21 05:35:35','Anonymous','x'),(5,'SQL Server','SQL Server is ...','2022-01-21 05:35:55','Anonymous','x'),(6,'MongoDB','MongoDB is ...','2022-01-21 05:36:12','Anonymous','x'),(7,'hello','this is web page','2022-01-21 05:38:45','Anonymous','x'),(12,'hello2','hello2...','2022-01-22 01:21:41','Anonymous','x'),(13,'test','dd','2022-02-04 20:34:53','YES','spoiler'),(14,'test2','this is test2.','2022-02-04 20:44:03','user1','spoiler'),(15,'test3','this is test3','2022-02-04 22:39:18','user1','spoiler'),(16,'test4','test4','2022-02-04 22:47:08','user1','');
+/*!40000 ALTER TABLE `community_text` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-12 23:49:15
+CREATE DATABASE  IF NOT EXISTS `siss_winter` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `siss_winter`;
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: siss_winter
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -41,88 +268,6 @@ LOCK TABLES `community_information` WRITE;
 INSERT INTO `community_information` VALUES (1,'first 문제','first ...','2022-01-21 19:10:14','Anonymous');
 /*!40000 ALTER TABLE `community_information` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `community_text`
---
-
-DROP TABLE IF EXISTS `community_text`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `community_text` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `author` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `spoiler` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT 'x',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `community_text`
---
-
-LOCK TABLES `community_text` WRITE;
-/*!40000 ALTER TABLE `community_text` DISABLE KEYS */;
-INSERT INTO `community_text` VALUES (3,'mysql','mysql is...','2022-01-21 05:35:00','Anonymous','x'),(4,'Oracle','Oracle is ...','2022-01-21 05:35:35','Anonymous','x'),(5,'SQL Server','SQL Server is ...','2022-01-21 05:35:55','Anonymous','x'),(6,'MongoDB','MongoDB is ...','2022-01-21 05:36:12','Anonymous','x'),(7,'hello','this is web page','2022-01-21 05:38:45','Anonymous','x'),(12,'hello2','hello2...','2022-01-22 01:21:41','Anonymous','x'),(13,'test','dd','2022-02-04 20:34:53','YES','spoiler'),(14,'test2','this is test2. aaa','2022-02-04 20:44:03','user1','spoiler'),(15,'test3','this is test3','2022-02-04 22:39:18','user1','spoiler'),(16,'test4','test4','2022-02-04 22:47:08','user1','');
-/*!40000 ALTER TABLE `community_text` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `gamelevel`
---
-
-DROP TABLE IF EXISTS `gamelevel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gamelevel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `answer` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gamelevel`
---
-
-LOCK TABLES `gamelevel` WRITE;
-/*!40000 ALTER TABLE `gamelevel` DISABLE KEYS */;
-INSERT INTO `gamelevel` VALUES (1,'1번','1번 문제','2022-02-04 23:13:28','정답'),(2,'2번','2번 문제','2022-02-04 23:14:22','정답'),(3,'3번','3번 문제','2022-02-04 23:14:26','정답'),(4,'4번','4번 문제','2022-02-04 23:14:30','정답'),(5,'5번','5번 문제','2022-02-04 23:14:33','정답'),(6,'6번','6번 문제','2022-02-04 23:14:39','정답');
-/*!40000 ALTER TABLE `gamelevel` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `gamelogin`
---
-
-DROP TABLE IF EXISTS `gamelogin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gamelogin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login_id` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `login_pw` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created` datetime NOT NULL DEFAULT current_timestamp(),
-  `level` int(11) NOT NULL DEFAULT 1,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gamelogin`
---
-
-LOCK TABLES `gamelogin` WRITE;
-/*!40000 ALTER TABLE `gamelogin` DISABLE KEYS */;
-INSERT INTO `gamelogin` VALUES (1,'admin','0000','2022-01-21 19:14:13',6),(2,'user1','1111','2022-02-04 19:43:27',4);
-/*!40000 ALTER TABLE `gamelogin` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +278,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-10  5:16:25
+-- Dump completed on 2022-02-12 23:49:14
