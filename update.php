2047ec753c990d31e37cd4c087cc57cddad1b36f
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:37:50 +0900

    Update update.php

--- update.php
+++ update.php
@@ -30,6 +30,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>SISS Winter Projext</title>
       <link rel="stylesheet" type="text/css" href="#"/>
