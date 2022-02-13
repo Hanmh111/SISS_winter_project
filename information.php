a261cfc11c6ec80aef237f06c8af75f408ff1107
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:33:08 +0900

    Update information.php

--- information.php
+++ information.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>SISS Winter Projext</title>
       <link rel="stylesheet" type="text/css" href="#"/>
       <script type="text/javascript" src=""></script>
   </head>
   <body>
-    <div><a href="index.php">home</a></div>
-    <div><a href="community.php"><h1>커뮤니티</h1></a></div>
+  <div class="centering"><a href="index.php"><img src="banner.jpg" width="150" height="150"></a>
+    <a href="community.php"><h1>커뮤니티</h1></a>
     <h3>문제 제보</h3>
     <form action="process_information.php" method="POST">
       <p><input type="text" name="title" placeholder="제목"></p>
       <p><textarea name="description" rows="8" cols="80" placeholder="문제를 제보해주셔서 감사합니다."></textarea></p>
       <p><input type="submit"></p>
+    </div>
   </body>
 </html>
