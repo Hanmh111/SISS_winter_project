037026a04beb8e190aaa7eb2921c2c1223b9fd4c
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:32:22 +0900

    Update game_description.php

--- game_description.php
+++ game_description.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>SISS Winter Projext</title>
       <link rel="stylesheet" type="text/css" href="#"/>
       <script type="text/javascript" src=""></script>
   </head>
   <body>
-    <div><a href="index.php">home</a></div>
+    <div class="centering"><a href="index.php"><img src="banner.jpg" width="150" height="150"></a>
     <h1>게임 설명</h1>
     <p>
       게임 설명 내용
     </p>
+  </div>
   </body>
 </html>
