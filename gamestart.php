86c0a8dda8e9e9712cb907536c4e45ee9a7d3d3b
seoyeonworld <96773185+seoyeonworld@users.noreply.github.com>
2022-02-14 00:32:35 +0900

    Update gamestart.php

--- gamestart.php
+++ gamestart.php
@@ -1,18 +1,21 @@
 <?php
 session_start();
-$is_logged=$_SESSION['is_logged'];
-if($is_logged=='YES'){
-  echo $_SESSION['userid'];
+if(isset($_SESSION['is_logged'])){
+  $is_logged = $_SESSION['is_logged']; ?>
+  <div style="color: white;"><?php
+  echo $_SESSION['userid'],"님 환영합니다.</br>"; ?></div><?php
   echo "<a href=process_logout.php>로그아웃</a>";
 }
-
 else{
+  ?>
+  <div style="color: white;"><?php
+  echo "로그인 먼저 해주세요"; ?></div>
+  <div class="right">
+    <a href="sign_in.php">회원가입</a>
+    <a href="login.php">로그인</a>
+  </div>  <?php
+;}
 ?>
-<script>
-alert("로그인해야 접근하실 수 있습니다.");
-location.replace("./index.php");
-</script>
-<?php } ?>
 <?php
   $conn = mysqli_connect(
     'localhost',
@@ -69,17 +72,67 @@ location.replace("./index.php");
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="utf-8">
     <title>SISS Winter Projext</title>
       <link rel="stylesheet" type="text/css" href="#"/>
       <script type="text/javascript" src=""></script>
   </head>
   <body>
-    <div><a href="index.php">home</a></div>
+    <div class="centering"><a href="index.php"><img src="banner.jpg" width="100" height="100"></a>
+    <p>
     <h1>문제</h1>
     <?=$list?>
     <h2><?=$game['title']?></h2>
     <p><?=$game['description']?></p>
+    <input id="firsthint" type="checkbox" />
+ <label for="firsthint">힌트1</label>
+   <div class="one">
+     <?php
+     $conn = mysqli_connect(
+       'localhost',
+       'root',
+       'qkrqhrja2',
+       'siss_winter');
+
+     if(isset($_GET['id'])){
+       $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+       $sql_id = "SELECT firsthint FROM gamelevel WHERE id={$filtered_id}";
+       $result_id = mysqli_query($conn, $sql_id);
+       $row = mysqli_fetch_array($result_id);
+       echo $row['firsthint']. "<br>";
+
+     }else{
+       echo "문제를 선택해주세요.";
+     }
+     mysqli_close($conn);
+  ?>
+
+   </div>
+   <input id="secondhint" type="checkbox" />
+   <label for="secondhint">힌트2</label>
+     <div class="two">
+       <?php
+       $conn = mysqli_connect(
+         'localhost',
+         'root',
+         'qkrqhrja2',
+         'siss_winter');
+
+       if(isset($_GET['id'])){
+         $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+         $sql_id = "SELECT secondhint FROM gamelevel WHERE id={$filtered_id}";
+         $result_id = mysqli_query($conn, $sql_id);
+         $row = mysqli_fetch_array($result_id);
+         echo $row['secondhint']. "<br>";
+
+       }else{
+       echo "문제를 선택해주세요.";
+       }
+       mysqli_close($conn);
+     ?>
+     </div>
     <p><?=$answer_link?></p>
+
   </body>
 </html>
